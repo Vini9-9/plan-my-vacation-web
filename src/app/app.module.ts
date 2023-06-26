2308c50e165ee271f306f629b53464dffa6664eb
Vinicius Pessoa <vinicius.silva3@ibm.com>
2023-06-26 14:20:13 -0300

    structure of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { InstructionsComponent } from './components/instructions/instructions.component';
+import { FormComponent } from './components/form/form.component';
+import { ResultsComponent } from './components/results/results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    InstructionsComponent,
+    FormComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
