e1e7c39e91b601c92fef2f5a457ba3b339db7d97
Vinicius Pessoa <vinicius.silva3@ibm.com>
2023-06-28 01:41:14 -0300

    ResultsComponent with mock

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -7,9 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
+  mock = {
+    "status": "OK",
+    "periodosIdeias": [
+      {
+        "qtdDias": 11,
+        "diaInicio": "28/08/2023",
+        "diaSemanaInicio": "Segunda",
+        "diaFim": "08/09/2023",
+        "diaSemanaFim": "Sexta",
+        "feriado": {
+          "nome": "Independência do Brasil",
+          "tipo": "feriado",
+          "nivel": "nacional"
+        }
+      },
+      {
+        "qtdDias": 11,
+        "diaInicio": "02/10/2023",
+        "diaSemanaInicio": "Segunda",
+        "diaFim": "13/10/2023",
+        "diaSemanaFim": "Sexta",
+        "feriado": {
+          "nome": "Nossa Senhora Aparecida",
+          "tipo": "feriado",
+          "nivel": "nacional"
+        }
+      },
+      {
+        "qtdDias": 11,
+        "diaInicio": "23/10/2023",
+        "diaSemanaInicio": "Segunda",
+        "diaFim": "03/11/2023",
+        "diaSemanaFim": "Sexta",
+        "feriado": {
+          "nome": "Finados",
+          "tipo": "feriado",
+          "nivel": "nacional"
+        }
+      },
+      {
+        "qtdDias": 11,
+        "diaInicio": "15/12/2023",
+        "diaSemanaInicio": "Sexta",
+        "diaFim": "26/12/2023",
+        "diaSemanaFim": "Terça",
+        "feriado": {
+          "nome": "Natal",
+          "tipo": "feriado",
+          "nivel": "nacional"
+        }
+      }
+    ]
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
