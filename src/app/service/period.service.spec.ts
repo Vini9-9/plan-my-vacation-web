b563cd4fd969fa625478627d93bb8af9bb57ea17
Vinicius Pessoa <vinicius.silva3@ibm.com>
2023-07-12 11:02:21 -0300

    Tests API periods

--- src/app/service/period.service.spec.ts
+++ src/app/service/period.service.spec.ts
@@ -3,8 +3,12 @@ import { TestBed } from '@angular/core/testing';
 import { PeriodService } from './period.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { PeriodRequest } from '../model/PeriodRequest';
-import { PERIODS } from 'src/mocks/periods-data';
-import { BODY_PERIODS } from 'src/mocks/body-data';
+import { PERIODS } from 'src/mocks/responses/periods-data';
+import { BODY_PERIODS } from 'src/mocks/requests/body-data';
+import { BODY_NO_DATA } from 'src/mocks/requests/body-no-data';
+import { NO_DATA } from 'src/mocks/responses/no-data';
+import { BODY_INVALID_DATE } from 'src/mocks/requests/body-invalid-date-data';
+import { INVALID_DATE } from 'src/mocks/responses/invalid-date';
 
 describe('PeriodService', () => {
   let service: PeriodService, httpTestingController: HttpTestingController;
@@ -34,6 +38,38 @@ describe('PeriodService', () => {
     expect(req.request.method).toBe('POST');
     req.flush(PERIODS);
 
+
+  });
+
+  it('return no periods', () => {
+    let body: PeriodRequest = BODY_NO_DATA;
+
+    service.getPeriods(body).subscribe((periods) => {
+      expect(periods).toBeTruthy();
+      expect(periods.status).toBe('OK')
+      expect(periods.periodosIdeias?.length).toBe(0)
+    })
+
+    const req = httpTestingController.expectOne(service.apiPeriods);
+
+    expect(req.request.method).toBe('POST');
+    req.flush(NO_DATA);
+
+  });
+
+  it('invalid body dates when return the periods', () => {
+    let body: PeriodRequest = BODY_INVALID_DATE;
+
+    service.getPeriods(body).subscribe((periods) => {
+      expect(periods).toBeTruthy();
+      expect(periods.status).toBe('error')
+      expect(periods.mensagem).toBe('Data inicio deve ser antes da data fim')
+    })
+
+    const req = httpTestingController.expectOne(service.apiPeriods);
+
+    expect(req.request.method).toBe('POST');
+    req.flush(INVALID_DATE);
 
   });
 
