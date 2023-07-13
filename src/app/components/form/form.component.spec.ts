055ee0261124fed15064a09d64d3f5107fff0702
Vinicius Pessoa <vinicius.silva3@ibm.com>
2023-07-13 00:35:37 -0300

    Initial test formComponent

--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -1,25 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
+import { LocationService } from 'src/app/service/location.service';
+import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
+import { PeriodService } from 'src/app/service/period.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing"
 
-describe('FormComponent', () => {
+fdescribe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      imports: [HttpClientTestingModule],
+      declarations: [ FormComponent ],
+      providers: [
+        LocationService,
+        DateAdapter,
+        { provide: MAT_DATE_LOCALE, useValue: 'pt'},
+        PeriodService
+      ]
     })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    .compileComponents().then(() => {
+      fixture = TestBed.createComponent(FormComponent);
+      component = fixture.componentInstance;
+    });
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should formatDateToBody less than ten', () => {
+  //   const date = new Date(2023, 8, 9);
+  //   const formattedDate = component.formatDateToBody(date);
+  //   expect(formattedDate).toEqual('2023-09-09');
+  // });
+
+  // it('should formatDateToBody greater than ten', () => {
+  //   const date = new Date(2023, 9, 14);
+  //   const formattedDate = component.formatDateToBody(date);
+  //   expect(formattedDate).toEqual('2023-10-14');
+  // });
+
+
 });
